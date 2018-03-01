c2164323115ba9a3bd6313ab1fa4f2eac8f3ab73
harry08 <ha.huebner@nexgo.de>
2018-03-01 23:52:29 +0100

    First implementation of UIPicker for setting max receiver per mail

--- Mailings/Settings/CommonSettingsViewController.swift
+++ Mailings/Settings/CommonSettingsViewController.swift
@@ -7,18 +7,26 @@
 
 import UIKit
 
-class CommonSettingsViewController: UITableViewController {
+class CommonSettingsViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var splitReceiverSwitch: UISwitch!
-    @IBOutlet weak var maxReceiverPerMail: UILabel!
+    @IBOutlet weak var maxReceiverPerMail: UITextField!
+    
+    var pickerDataSource = ["1", "2", "3", "4", "5"];
+    var resultString = ""
     
     var settingsController : CommonSettingsController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        maxReceiverPerMail.inputView = pickerView
+        
         settingsController = CommonSettingsController.sharedInstance
-        fillControls()
+        fillControls()        
     }
 
     /**
@@ -29,8 +37,35 @@ class CommonSettingsViewController: UITableViewController {
         maxReceiverPerMail.text = String(settingsController!.getMaxReceiver())
     }
     
+    // MARK: - Actions
+    
     @IBAction func splitReceiverSwitchChanged(_ sender: Any) {
         let splitReceiver = splitReceiverSwitch.isOn
         settingsController!.setSplitReceivers(splitReceiver)
+    }
+    
+    // MARK: - Picker Delegate
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerDataSource[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        let nrOfReceiver = row + 1
+        settingsController!.setMaxReveiver(nrOfReceiver)
+        maxReceiverPerMail.text = String(settingsController!.getMaxReceiver())
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
 }
